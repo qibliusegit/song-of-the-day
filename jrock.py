035fa1467d2377c392d0d365a073314f72079887
Dhruv Ravinuthala <dhruv.ravinuthala@gmail.com>
2026-06-03 01:38:40 -0500

    Added the beginning of the randomization code. For now, it just picks two random letters and then combines them, and then prints them to the terminal.

--- jrock.py
+++ jrock.py
@@ -1,7 +1,11 @@
 # i have NO IDEA what i'm doing tbh #
 
+letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
 BOT_TOKEN = "YOUR_TOKEN_HERE"
 import discord
+import requests
+import random
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -21,6 +25,21 @@ async def on_message(message):
 
     if message.content.startswith('j-rock') or message.content.startswith('jrock'):
         await message.channel.send('its me, your friendly neighborhood jrock enthusiast!')
+    
+    if message.content.startswith('larp'):
+        await message.channel.send('im always larping, except for jrock, i love jrock')
+
+    if message.content("?SOOD"):
+        daily_song()
+
+    def daily_song():
+        char1 = random.randint(0,25)
+        char2 = random.randint(0,25)
+        let1 = letter[char1]
+        let2 = letter[char2]
+        query = let1 + let2
+        print(query)
+
 
 
 
