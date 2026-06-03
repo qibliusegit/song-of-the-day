e2a3e730212929c840d9481371fcb1e5d385cd56
Dhruv Ravinuthala <dhruv.ravinuthala@gmail.com>
2026-06-03 02:31:28 -0500

    Fixed some basic bugs, made it so the bot would respond when pinged and if a keyword was in a message at all with any casing rather than being specific, and made it call the musicbrainzapi for a work based on the random characters it generates.

--- jrock.py
+++ jrock.py
@@ -1,3 +1,14 @@
+def daily_song():
+    char1 = random.randint(0,25)
+    char2 = random.randint(0,25)
+    let1 = letter[char1]
+    let2 = letter[char2]
+    query = let1 + let2
+    print(query)
+    song_search = f"https://musicbrainz.org/ws/2/work/?query={query}"
+    song = requests.get(song_search)
+    print(song.text)
+
 letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 BOT_TOKEN = "YOUR_TOKEN_HERE"
@@ -18,25 +29,20 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('natori'):
+    if 'natori' in message.content.lower():
         await message.channel.send('natori is my goat i love polargeist wait no thats geometry dash i mean poltergeist')
 
-    if message.content.startswith('j-rock') or message.content.startswith('jrock'):
+    if 'jrock' in message.content.lower() or "j-rock" in message.content.lower():
         await message.channel.send('its me, your friendly neighborhood jrock enthusiast!')
     
-    if message.content.startswith('larp'):
+    if "larp" in message.content.lower():
         await message.channel.send('im always larping, except for jrock, i love jrock')
 
     if message.content == '?SOOD':
         daily_song()
 
-    def daily_song():
-        char1 = random.randint(0,25)
-        char2 = random.randint(0,25)
-        let1 = letter[char1]
-        let2 = letter[char2]
-        query = let1 + let2
-        print(query)
+    if client.user.mentioned_in(message):
+        await message.channel.send('hi do you wanna talk about jrock i really like jrock')
 
 
 
